117fcca1fd03b5ee50aed1c5df7640191e02f72b
Hannah Obote <ao223ir@student.lnu.se>
2023-09-11 23:00:16 +0200

    cord origin changes

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(logger('dev'))
 
 app.use(express.json())
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: ['http://localhost:3000', 'https://wt2-viz.vercel.app/'] }))
 
 app.get('/', function (req, res) {
   res.send(elasticResult)
